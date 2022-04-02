527646a1dfe80904ea45ed38770ce884e7e5a523
WebDeveloper7611 <gsattorov0102@mail.ru>
2022-04-02 15:53:25 +0500

    s

--- components/products/product_container.js
+++ components/products/product_container.js
@@ -4,8 +4,8 @@ import MyCard from "./product_card";
 const Products = ({products})=>{
     
     return<>
-        <h1 style={{textAlign: 'center' , marginTop: '50px'}}>Products </h1>      
-        <div className={styles.container} id='product'>
+        <h1 id='product' style={{textAlign: 'center' , marginTop: '50px'}}>Products </h1>      
+        <div className={styles.container} >
 
     {
         products.map((item,index)=>(<MyCard product={item} key={index} />))
